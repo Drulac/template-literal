a3dd7b865a6385eadec5ff404dde5d4e94c1fdd4
drulac <drulac@protonmail.com>
2020-10-16 23:36:26 +0200

    review

--- compile.js
+++ compile.js
@@ -1,3 +1,8 @@
-module.exports = function(templateStr) {
-	return new Function('d', 'return `' + templateStr + '`')
+module.exports = function (templateStr) {
+	return new Function(
+		'd',
+		'const include = (file, opts={})=>d.include(file+".tl", Object.assign(d, opts)); return `' +
+			templateStr +
+			'`'
+	)
 }
